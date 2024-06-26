efa2f41a5059fabf53df6b07aa6b2357777583ec
Matt Lavoie <mathieulavoie94@gmail.com>
2024-06-26 16:16:53 -0400

    update to latest deps

--- stylelint/index.js
+++ stylelint/index.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-prettier",
-    "stylelint-order",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-order"],
   plugins: ["stylelint-scss", "stylelint-order"],
   rules: {
     "color-named": [
@@ -15,14 +11,13 @@ module.exports = {
     "color-hex-length": null,
     "order/order": ["custom-properties", "declarations"],
     "order/properties-alphabetical-order": true,
-    "string-quotes": "double",
     "max-nesting-depth": 3,
     "selector-no-qualifying-type": null,
     "alpha-value-notation": "number",
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
     "no-descending-specificity": null,
-    "function-no-unknown": [true, {ignoreFunctions: ["lighten", "darken"]}]
+    "function-no-unknown": [true, { ignoreFunctions: ["lighten", "darken"] }],
   },
   customSyntax: "postcss-scss",
 };
